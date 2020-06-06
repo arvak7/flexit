bf4170c8c14f85893c83d674f6f52d096256a5ff
mgarcia <mgarcia@hiberus.com>
2020-06-06 14:47:20 +0200

    add test methods

--- backoffice/src/main/java/com/flexit/controller/EmployeeController.java
+++ backoffice/src/main/java/com/flexit/controller/EmployeeController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -14,10 +13,7 @@ import java.util.List;
 public class EmployeeController {
 
     public List<EmployeeDTO> getAllEmployees() {
-        EmployeeDTO employeeDTO = new EmployeeDTO();
-        employeeDTO.setName("Pep");
-        employeeDTO.setLastEventDate("1985-04-23");
-        return Arrays.asList(employeeDTO);
+        return null;
     }
 
 }
